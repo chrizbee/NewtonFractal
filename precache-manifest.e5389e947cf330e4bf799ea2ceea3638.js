08ad0e55c21cae1512d8cd36ad0cc065887cf110
Christian <chr.bauer001@gmail.com>
2019-08-05 10:28:44 +0200

    Updates

---
+++ precache-manifest.e5389e947cf330e4bf799ea2ceea3638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381e4e751eb5a325461711b83f1301e0",
+    "url": "/NewtonFractal/index.html"
+  },
+  {
+    "revision": "dcb7f4c2afcad2ef0fda",
+    "url": "/NewtonFractal/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/NewtonFractal/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "dcb7f4c2afcad2ef0fda",
+    "url": "/NewtonFractal/static/js/main.707dc51b.chunk.js"
+  },
+  {
+    "revision": "1910e11715f5ffef2a45",
+    "url": "/NewtonFractal/static/js/runtime~main.29151420.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/NewtonFractal/static/media/logo.5d5d9eef.svg"
+  }
+]);
